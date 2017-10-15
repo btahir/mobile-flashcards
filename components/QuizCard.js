7d31778c0d3e190c9df6f60d3d802aca952e0306
btahir <bilal@Bilals-iMac.local>
2017-10-15 17:27:20 -0400

    can swipe cards
    can swipe cards

--- components/QuizCard.js
+++ components/QuizCard.js
@@ -1,6 +1,15 @@
 import React from 'react'
-import { Text, View, Animated, PanResponder } from 'react-native'
+import { 
+	Text, 
+	View, 
+	Animated, 
+	PanResponder,
+	Dimensions } from 'react-native'
 import { connect } from 'react-redux'
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 class QuizCard extends React.Component {
 	constructor(props) {
@@ -13,16 +22,52 @@ class QuizCard extends React.Component {
 			onPanResponderMove: (event, gesture) => {
 				position.setValue({x: gesture.dx, y: gesture.dy});
 			},
-			onPanResponderRelease: () => {},
+			onPanResponderRelease: (event, gesture) => {
+				if(gesture.dx > SWIPE_THRESHOLD) {
+					this.forceSwipe('right');
+				} else if (gesture.dx < -SWIPE_THRESHOLD) {
+					this.forceSwipe('left');
+				} else {
+					this.resetPosition();
+				}
+			},
 		})
 
 		this.state = { panResponder, position };
 	}
 
+	forceSwipe(direction) {
+		const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+		Animated.timing(this.state.position, {
+			toValue: { x, y: 0 },
+			duration: SWIPE_OUT_DURATION,
+		}).start(() => this.onSwipeComplete(direction));
+	}
+
+	onSwipeComplete(direction) {
+		const { onSwipeRight, onSwipeLeft } = this.props;
+
+		direction === 'right' ? onSwipeRight() : onSwipeLeft();
+
+
+	}
+
+	resetPosition() {
+		Animated.spring(this.state.position, {
+			toValue: { x: 0, y: 0 }
+		}).start();
+	}
+
 	getCardStyle() {
+		const { position } = this.state;
+		const rotate = position.x.interpolate({
+			inputRange: [-SCREEN_WIDTH * 2.0, 0, SCREEN_WIDTH * 2.0],
+			outputRange: ['-120deg', '0deg', '120deg']
+		})
+
 		return {
-			...this.state.position.getLayout(),
-			transform: [{ rotate: '45deg'}],
+			...position.getLayout(),
+			transform: [{ rotate }],
 		};
 	}
 
